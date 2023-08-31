59f757c3c1863093ec2c56d35758ddcffab6ec54
Nicolò Ciraci <nicolo.ciraci@elmec.it>
2023-08-31 16:25:45 +0200

    Remove complexity

--- alertmanager_telegram/templating.py
+++ alertmanager_telegram/templating.py
@@ -5,21 +5,6 @@ from alertmanager_telegram.logging import logger
 
 
 def render(content):
-    alerts = content["alerts"]
-
-    common_labels = content.get("commonLabels", {})
-    common_annotations = content.get("commonAnnotations", {})
-    for alert in alerts:
-        if "labels" in alert:
-            labels = alert["labels"]
-            for label in common_labels.keys():
-                labels.pop(label, None)
-
-        if "annotations" in alert:
-            annotations = alert["annotations"]
-            for annotation in common_annotations.keys():
-                annotations.pop(annotation, None)
-
     message = render_template("default.html", **content)
     while len(message) > MAX_MESSAGE_LENGTH and len(content["alerts"]) > 0:
         logger.debug("Truncating alerts list (with ellipsis)")
